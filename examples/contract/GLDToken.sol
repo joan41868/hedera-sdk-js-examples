726e17788e95a49490e8d595ebcbb59247ac8a13
alshapi <alexander.shapkarov@limechain.tech>
2021-12-21 10:31:46 +0200

    add mint function

--- examples/contract/GLDToken.sol
+++ examples/contract/GLDToken.sol
@@ -1,4 +1,3 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -7,5 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract GLDToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint() public {
+      _mint(msg.sender, 1000 * 10**18);
     }
 }
